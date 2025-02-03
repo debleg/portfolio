7cb1d01f60a21f36ec00555cabbcf2d2ca89c90f
degleg <legoff.d@outlook.com>
2025-02-03 18:43:50 +0100

    Implement basic theme switch

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
 "use strict";
-console.log("test");
+//Theme handling based on toggle switch
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+//If theme preference is stored, apply on reload
+const theme = localStorage.getItem('theme');
+if (theme) {
+    body.classList.add(theme);
+}
+//on click the class is changed and the user choice saved in local storage
+themeToggle === null || themeToggle === void 0 ? void 0 : themeToggle.addEventListener("click", () => {
+    if (body.classList.contains("light")) {
+        body.classList.replace("light", "dark");
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        body.classList.replace("dark", "light");
+        localStorage.setItem('theme', 'light');
+    }
+});
 //# sourceMappingURL=index.js.map
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,1 +1,22 @@
-console.log("test");
+
+//Theme handling based on toggle switch
+const themeToggle = document.getElementById("theme-toggle") as HTMLElement | null;
+const body = document.body;
+
+//If theme preference is stored, apply on reload
+const theme = localStorage.getItem('theme')
+if (theme) {
+    body.classList.add(theme)
+}
+
+//on click the class is changed and the user choice saved in local storage
+themeToggle?.addEventListener("click",  () => {
+    if (body.classList.contains("light")) {
+        body.classList.replace("light", "dark")
+        localStorage.setItem('theme', 'dark')
+    } else {
+        body.classList.replace("dark", "light")
+        localStorage.setItem('theme', 'light')
+    }
+
+})
