b59eaee07b685e54bbf8f6586da62ef4fa50e261
degleg <legoff.d@outlook.com>
2025-02-05 20:26:40 +0100

    Run linters on ts file

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,22 +1,22 @@
-
 //Theme handling based on toggle switch
-const themeToggle = document.getElementById("theme-toggle") as HTMLElement | null;
+const themeToggle = document.getElementById(
+  "theme-toggle"
+) as HTMLElement | null;
 const body = document.body;
 
 //If theme preference is stored, apply on reload
-const theme = localStorage.getItem('theme')
+const theme = localStorage.getItem("theme");
 if (theme) {
-    body.classList.add(theme)
+  body.classList.add(theme);
 }
 
 //on click the class is changed and the user choice saved in local storage
-themeToggle?.addEventListener("click",  () => {
-    if (body.classList.contains("light")) {
-        body.classList.replace("light", "dark")
-        localStorage.setItem('theme', 'dark')
-    } else {
-        body.classList.replace("dark", "light")
-        localStorage.setItem('theme', 'light')
-    }
-
-})
+themeToggle?.addEventListener("click", () => {
+  if (body.classList.contains("light")) {
+    body.classList.replace("light", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.replace("dark", "light");
+    localStorage.setItem("theme", "light");
+  }
+});
