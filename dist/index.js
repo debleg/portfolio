be7485523b9a8283230ff4571ab681ec3c745e2f
degleg <legoff.d@outlook.com>
2025-02-07 21:35:31 +0100

    Setup i18n translation base

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 //Theme handling based on toggle switch
 const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
 //If theme preference is stored, apply on reload
-const theme = localStorage.getItem('theme');
+const theme = localStorage.getItem("theme");
 if (theme) {
     body.classList.add(theme);
 }
@@ -11,11 +20,56 @@ if (theme) {
 themeToggle === null || themeToggle === void 0 ? void 0 : themeToggle.addEventListener("click", () => {
     if (body.classList.contains("light")) {
         body.classList.replace("light", "dark");
-        localStorage.setItem('theme', 'dark');
+        localStorage.setItem("theme", "dark");
     }
     else {
         body.classList.replace("dark", "light");
-        localStorage.setItem('theme', 'light');
+        localStorage.setItem("theme", "light");
     }
 });
+//I18n setup
+//the default language, French for now
+const defaultLocale = "fr";
+//the language the site will be translated to
+let locale;
+//the translations relocated to json files
+let translations = {};
+//as the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    setLocale(defaultLocale);
+});
+function setLocale(newLocale) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (newLocale === locale)
+            return;
+        const newTranslations = yield fetchTranslationsFor(newLocale);
+        locale = newLocale;
+        translations = newTranslations;
+        translatePage();
+    });
+}
+//translations are retrieved from the corresponding json file
+function fetchTranslationsFor(newLocale) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(`../src/lang/${newLocale}.json`);
+        return yield response.json();
+    });
+}
+function translatePage() {
+    document
+        .querySelectorAll("[data-i18n-key]")
+        .forEach(translateElement);
+}
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    if (key === null)
+        return;
+    const translation = translations[key];
+    if (translation) {
+        element.innerText = translation;
+    }
+    else {
+        console.warn(`Translation missing for key: ${key}"`);
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -20,3 +20,50 @@ themeToggle?.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+
+//I18n setup
+
+//the default language, French for now
+const defaultLocale: string = "fr";
+//the language the site will be translated to
+let locale: string;
+
+interface Translations {
+  [key: string]: string;
+}
+//the translations relocated to json files
+let translations: Translations = {};
+
+//as the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  setLocale(defaultLocale);
+});
+
+async function setLocale(newLocale: string) {
+  if (newLocale === locale) return;
+  const newTranslations = await fetchTranslationsFor(newLocale);
+  locale = newLocale;
+  translations = newTranslations;
+  translatePage();
+}
+//translations are retrieved from the corresponding json file
+async function fetchTranslationsFor(newLocale: string): Promise<Translations> {
+  const response = await fetch(`../src/lang/${newLocale}.json`);
+  return await response.json();
+}
+function translatePage() {
+  document
+    .querySelectorAll<HTMLElement>("[data-i18n-key]")
+    .forEach(translateElement);
+}
+function translateElement(element: HTMLElement) {
+  const key: string | null = element.getAttribute("data-i18n-key");
+  if (key === null) return;
+
+  const translation = translations[key];
+  if (translation) {
+    element.innerText = translation;
+  } else {
+    console.warn(`Translation missing for key: ${key}"`);
+  }
+}
